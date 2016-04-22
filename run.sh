3b950e556335c262718f5eb7f8e5a8a8e7409510
saga92 <yahao9210normal@126.com>
2016-04-22 15:23:02 +0800

    add alembic -c

--- run.sh
+++ run.sh
@@ -1,17 +1,20 @@
 #! /bin/bash
 
+source ./migration.cfg
+mysql -h${db_host} -u${db_username} -p${db_password} < ./alembic.sql
+
 while [[ $# > 0 ]]
 do
     key="$1"
     case $key in
     -up|--upgrade)
         VERSION="$2"
-        alembic upgrade ${VERSION}
+        alembic -c ./alembic.ini upgrade ${VERSION}
         shift
         ;;
     -down|--downgrade)
         VERSION="$2"
-        alembic downgrade ${VERSION}
+        alembic -c ./alembic.ini downgrade ${VERSION}
         shift
         ;;
     -h|--help)
